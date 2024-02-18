5e801d195e378c1caa7f7bde901926ad3b37d2b6
ignorantshr <shrshraa@outlook.com>
2024-02-18 15:23:09 +0800

    to chatpter 11

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -38,6 +38,9 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
