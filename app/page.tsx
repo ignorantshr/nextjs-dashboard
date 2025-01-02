c8b57c222327249a797c02d05003753f9b267d6c
ignorantshr <shrshraa@outlook.com>
2025-01-02 19:29:58 +0800

    font and image

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import style from '@/app/ui/home.module.css';
-  
+import { lusitana } from "@/app/ui/fonts";
+import Image from "next/image";
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -15,7 +17,7 @@ export default function Page() {
           className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
         />
         <div className={style.shape} />
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -31,6 +33,22 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            layout="responsive" // 设置为响应式布局
+            alt="Hero Image"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            layout="responsive" // 设置为响应式布局
+            alt="Hero Image"
+          />
         </div>
       </div>
     </main>
